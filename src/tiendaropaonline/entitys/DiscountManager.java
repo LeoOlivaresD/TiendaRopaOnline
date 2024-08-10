3e2e9a6213c029cb45e50915607d1a395ebd98c7
LeoOlivaresD <cspraword@gmail.com>
2024-08-09 22:04:19 -0400

    Termino delo propuesto por la pauta

--- src/tiendaropaonline/entitys/DiscountManager.java
+++ src/tiendaropaonline/entitys/DiscountManager.java
@@ -1,5 +1,8 @@
 package tiendaropaonline.entitys;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DiscountManager {
     //Declaracion de la instancia privada y estatica de la propia clase
     private static DiscountManager instance;
@@ -7,8 +10,28 @@ public class DiscountManager {
     //Constructor privado de mi clase
     private DiscountManager() {
     }
+    //Atributos unicos de mi clase para concepto de pruebas en el main
+    private String descuentos = "30%";
+    private List<String> listaArticulos = new ArrayList<>();
+    // Getters & Setters
+    public String getDescuentos() {
+        return descuentos;
+    }
+
+    public void setDescuentos(String descuentos) {
+        this.descuentos = descuentos;
+    }
+
+    public List<String> getListaArticulos() {
+        return listaArticulos;
+    }
+
+    public void setListaArticulos(List<String> listaArticulos) {
+        this.listaArticulos = listaArticulos;
+    }
     
-    //Cracion de metodo statico
+    
+    //Cracion de metodo statico ñpara retornar mi objeto y en caso de ser nulo , se creara una instancia
     public static DiscountManager getInstance() {
         if (instance == null) {
             instance = new DiscountManager();
